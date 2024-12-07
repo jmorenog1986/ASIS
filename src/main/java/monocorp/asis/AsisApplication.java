ff7cfe967ac2c1d6ee15572f9ca42347c5d7194f
jmorenog1986 <jhonmorenogarcia@gmail.com>
2024-12-07 17:28:09 -0500

    Crar proyecto Prueba ASIS

---
+++ src/main/java/monocorp/asis/AsisApplication.java
@@ -0,0 +1,13 @@
+package monocorp.asis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsisApplication.class, args);
+    }
+
+}
